893017477913a510ead19637d058dfb92ceb0a44
ringmaster <a_github@midnightcircus.com>
2013-03-01 12:12:17 -0500

    Unbreak the javascript

--- extendedlog.plugin.php
+++ extendedlog.plugin.php
@@ -7,26 +7,28 @@ class ExtendedLog extends Plugin
 		$url = URL::get('auth_ajax', 'context=extendedlog');
 
 		$script = <<< SCRIPT
-var initi = itemManage.initItems;
-itemManage.initItems = function(){
-	initi();
-	$('.page-logs .manage .item .less,.page-logs .manage .item .message.minor').hide();
-	$('.page-logs .manage .item .more').show().css({clear: 'both', marginLeft: '40px', fontWeight: 'bold', width: '100%'});
-	$('.page-logs .manage .item').click(function(){
-		$('.extendedlog').remove();
-		$(this).after('<div class="extendedlog"><div class="textarea" style="white-space:pre;font-family:consolas,courier new,monospace;border:1px solid #999;padding:20px;margin:20px 0px;height:100px;overflow-y:auto;">Loading...</div></div>');
-		$('.extendedlog .textarea').resizeable();
-		$.post(
-			'{$url}',
-			{
-				log_id: $('.checkbox input', $(this)).attr('id').match(/\[([0-9]+)\]/)[1]
-			},
-			function(result){
-				$('.extendedlog .textarea').html(result)
-			}
-		);
-	});
-}
+$(function(){
+	var initi = itemManage.initItems;
+	itemManage.initItems = function(){
+		initi();
+		$('.page-logs .manage .item .less,.page-logs .manage .item .message.minor').hide();
+		$('.page-logs .manage .item .more').show().css({clear: 'both', marginLeft: '40px', fontWeight: 'bold', width: '100%'});
+		$('.page-logs .manage .item').click(function(){
+			$('.extendedlog').remove();
+			$(this).after('<div class="extendedlog"><div class="textarea" style="white-space:pre;font-family:consolas,courier new,monospace;border:1px solid #999;padding:20px;margin:20px 0px;height:100px;overflow-y:auto;">Loading...</div></div>');
+			$('.extendedlog .textarea').resizeable();
+			$.post(
+				'{$url}',
+				{
+					log_id: $('.checkbox input', $(this)).attr('id').match(/\[([0-9]+)\]/)[1]
+				},
+				function(result){
+					$('.extendedlog .textarea').html(result)
+				}
+			);
+		});
+	}
+});
 SCRIPT;
 		Stack::add('admin_header_javascript', $script, 'extendedlog', array('jquery', 'admin'));
 	}
